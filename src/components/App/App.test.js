064fdf85b78d09e1010cefbc98d03288720b349c
AkyunaAkish <akyunaakish@gmail.com>
2018-05-19 10:56:59 -0600

    initial test written

---
+++ src/components/App/App.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+import { shallow } from 'enzyme';
+
+import App from './App';
+
+const app = shallow(<App />);
+
+it('renders <App /> correctly', () => {
+    expect(app).toMatchSnapshot();
+});
+
