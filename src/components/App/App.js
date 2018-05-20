57c339a41f40cdc747426891cb874e0600872157
AkyunaAkish <akyunaakish@gmail.com>
2018-05-19 22:36:36 -0600

    added more test cases for app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,33 @@
 import React, { PureComponent } from 'react';
+import { Button } from 'react-bootstrap';
 
 class App extends PureComponent {
+    state = {
+        gifts: []    
+    }
+
+    addGift = () => {
+        const { gifts } = this.state;
+
+        const ids = this.state.gifts.map((gift => gift.id));
+        
+        // spread id array as args and get highest number in ids arr
+        const maxId = ids.length > 0 ? Math.max(...ids) : 0; 
+
+        gifts.push({ id: maxId+1 });
+
+        // update gifts with new modified copy
+        this.setState({ gifts });
+    }
+
     render() {
         return (
             <div className='app'>
-                <h1>App</h1>
+                <h1>Gift Giver</h1>
+                <Button className='btn-add'
+                        onClick={ this.addGift }>
+                    Add Gift
+                </Button>
             </div>
         );
     }
--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,17 +1,29 @@
 import React from 'react';
-
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-configure({ adapter: new Adapter() });
-
 import { shallow } from 'enzyme';
 
 import App from './App';
-
 const app = shallow(<App />);
 
+// Whenever you make a change to the app component
+// that you want to keep, go to the new failing test
+// and press "U" in the command line to update your snapshoit
+
 it('renders <App /> correctly', () => {
+    // snapshots keep a recorded history of 
+    // your react component
+    // whenever a change happens in the app
+    // snapshots will update and keep a history
+    // of the rendering of the component
     expect(app).toMatchSnapshot();
+});
+
+it('initializes <App /> `state` with an empty list of gifts', () => {
+    expect(app.state().gifts).toEqual([]);
+});
+
+it('adds a new gift to  <App /> `state` when clicking `add gift` button', () => {
+    app.find('.btn-add').simulate('click');
+
+    expect(app.state().gifts).toEqual([{ id: 1 }]);
 });
 
